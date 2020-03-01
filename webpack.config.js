e21cde029f7bc8a2fcd5afd42cc104e08046d0df
Ushinji <siguma971@gmail.com>
2020-03-01 19:52:32 +0900

    Fix by prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,66 @@
-const path = require("path");
-const { VueLoaderPlugin } = require("vue-loader");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env = {}) => ({
-  mode: env.prod ? "production" : "development",
-  devtool: env.prod ? "source-map" : "cheap-module-eval-source-map",
-  entry: path.resolve(__dirname, "./src/main.ts"),
+  mode: env.prod ? 'production' : 'development',
+  devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
+  entry: path.resolve(__dirname, './src/main.ts'),
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "/dist/"
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
   },
   resolve: {
-    extensions: [".js", ".ts", ".vue", ".css"],
+    extensions: ['.js', '.ts', '.vue', '.css'],
     alias: {
-      vue: "@vue/runtime-dom",
-      "@": path.resolve(__dirname, "src")
-    }
+      vue: '@vue/runtime-dom',
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         options: {
-          appendTsSuffixTo: [/\.vue$/]
-        }
+          appendTsSuffixTo: [/\.vue$/],
+        },
       },
       {
         test: /\.vue$/,
-        use: "vue-loader"
+        use: 'vue-loader',
       },
       {
         test: /\.png$/,
         use: {
-          loader: "url-loader",
-          options: { limit: 8192 }
-        }
+          loader: 'url-loader',
+          options: { limit: 8192 },
+        },
       },
       {
         test: /\.css$/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: { hmr: !env.prod }
+            options: { hmr: !env.prod },
           },
-          "css-loader"
-        ]
-      }
-    ]
+          'css-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].css"
-    })
+      filename: '[name].css',
+    }),
   ],
   devServer: {
     inline: true,
     hot: true,
-    stats: "minimal",
+    stats: 'minimal',
     contentBase: __dirname,
-    overlay: true
-  }
+    overlay: true,
+  },
 });
