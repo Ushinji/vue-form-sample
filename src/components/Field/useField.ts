f36def723dd365b39fa30bea9d2acdfed2f95151
Ushinji <siguma971@gmail.com>
2020-03-01 19:37:01 +0900

    Add useField

---
+++ src/components/Field/useField.ts
@@ -0,0 +1,33 @@
+import { ref, computed } from "vue";
+
+const useField = (
+  initialValue: string,
+  validate: (value: string) => boolean = () => false
+) => {
+  const value = ref(initialValue);
+  const isTouched = ref(false);
+
+  const error = computed(() => {
+    return !validate(value.value);
+  });
+
+  const onInput = (event: InputEvent) => {
+    if (event.target instanceof HTMLInputElement) {
+      value.value = event.target.value;
+    }
+  };
+
+  const onBlur = () => {
+    isTouched.value = true;
+  };
+
+  return {
+    props: { value, onInput, onBlur },
+    meta: {
+      isTouched,
+      error
+    }
+  };
+};
+
+export default useField;
